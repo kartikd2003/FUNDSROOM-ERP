e28519389f69b72b1197362f2401e2160dbe50a1
kartikd2003 <kartikdeshmukh2003@gmail.com>
2026-07-22 01:43:46 +0530

    Remove withCredentials from axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,11 +3,11 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "",
+  baseURL: import.meta.env.VITE_API_URL || '',
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 export default api;
